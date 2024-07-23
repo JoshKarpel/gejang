ec17b1636327dff41a03049dab869f49206b7d82
Josh Karpel <josh.karpel@gmail.com>
2024-07-23 00:25:21 -0500

    progress on line numbers

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,45 @@ use strum_macros::{AsRefStr, IntoStaticStr};
 
 fn main() {
     let chunk = Chunk {
-        code: vec![OpCode::Return],
+        code: vec![OpCode::Constant { index: 0 }, OpCode::Return],
+        constants: vec![1.0],
+        lines: vec![1, 2],
     };
     chunk.disassemble("test chunk");
 }
 
 #[derive(Debug, AsRefStr, IntoStaticStr)]
 enum OpCode {
+    Constant { index: u8 },
     Return,
 }
 
+type Value = f64;
 struct Chunk {
     code: Vec<OpCode>,
+    constants: Vec<Value>,
+    lines: Vec<u64>,
 }
 
 impl Chunk {
     fn disassemble(&self, name: &str) {
         println!("== {} ==", name);
 
-        let ops = self.code.iter().enumerate();
+        let ops = self.code.iter().zip(self.lines.iter()).enumerate();
 
-        for (offset, op) in ops {
+        for (offset, (op, line)) in ops {
+            let l = line;
+            let o = op.as_ref().to_ascii_uppercase();
+
             match op {
                 OpCode::Return => {
-                    println!("{:04} {}", offset, op.as_ref());
+                    println!("{offset:04} {l:04} {o}");
+                }
+                OpCode::Constant { index } => {
+                    println!(
+                        "{offset:04} {l:04} {o} {:?}",
+                        self.constants[*index as usize]
+                    );
                 }
             }
         }
