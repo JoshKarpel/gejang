7f8bbabd8c9bd69cb21a44461438c92e66b02ab7
Josh Karpel <josh.karpel@gmail.com>
2024-07-22 23:51:16 -0500

    basic disassemble

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::convert::AsRef;
+use strum_macros::{AsRefStr, IntoStaticStr};
+
 fn main() {
-    println!("Hello, world!");
+    let chunk = Chunk {
+        code: vec![OpCode::Return],
+    };
+    chunk.disassemble("test chunk");
+}
+
+#[derive(Debug, AsRefStr, IntoStaticStr)]
+enum OpCode {
+    Return,
+}
+
+struct Chunk {
+    code: Vec<OpCode>,
+}
+
+impl Chunk {
+    fn disassemble(&self, name: &str) {
+        println!("== {} ==", name);
+
+        let ops = self.code.iter().enumerate();
+
+        for (offset, op) in ops {
+            match op {
+                OpCode::Return => {
+                    println!("{:04} {}", offset, op.as_ref());
+                }
+            }
+        }
+    }
 }
