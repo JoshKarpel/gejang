fcfe971a8389596d972b84decb2657adf1ef8880
Josh Karpel <josh.karpel@gmail.com>
2024-07-29 22:02:05 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,45 @@
 use anyhow::Result;
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
 
 mod bytecode;
 
-#[derive(Parser, Debug)]
+#[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct CLI {
     #[command(subcommand)]
     command: Commands,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Debug, Subcommand)]
 enum Commands {
     /// Run the tree-walking interpreter
-    TreeWalker {},
+    TreeWalker(TreeWalkerArgs),
     /// Run the bytecode virtual machine interpreter
     Bytecode {},
+}
+
+#[derive(Debug, Args)]
+struct TreeWalkerArgs {
+    #[command(subcommand)]
+    command: TreeWalkerCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum TreeWalkerCommands {
+    Run { script: PathBuf },
 }
 
 fn main() -> Result<()> {
     let args = CLI::parse();
 
     match args.command {
-        Commands::TreeWalker {} => {
-            println!("Tree-Walker");
-            Ok(())
-        }
+        Commands::TreeWalker(args) => match args.command {
+            TreeWalkerCommands::Run { script } => {
+                println!("Tree-walker {}", script.display());
+                Ok(())
+            }
+        },
         Commands::Bytecode {} => bytecode::run(),
     }
 }
