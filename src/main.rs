9b71f4072596bf1af4b2902d92da42758d05ed4d
Josh Karpel <josh.karpel@gmail.com>
2024-07-29 22:25:25 -0500

    CLI over different interpreters (#7)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,60 @@
 use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
 
 mod bytecode;
 
-fn main() -> Result<()> {
-    bytecode::run()?;
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    Ok(())
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Run the tree-walking interpreter
+    #[command(alias = "tw")]
+    TreeWalker(TreeWalkerArgs),
+    /// Run the bytecode virtual machine interpreter
+    #[command(alias = "bc")]
+    #[command(alias = "vm")]
+    Bytecode(ByteCodeArgs),
+}
+
+#[derive(Debug, Args)]
+struct TreeWalkerArgs {
+    #[command(subcommand)]
+    command: TreeWalkerCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum TreeWalkerCommands {
+    /// Execute a script.
+    Run { script: PathBuf },
+}
+
+#[derive(Debug, Args)]
+struct ByteCodeArgs {
+    #[command(subcommand)]
+    command: ByteCodeCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum ByteCodeCommands {
+    /// Execute a script.
+    Run { script: PathBuf },
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::TreeWalker(args) => match args.command {
+            TreeWalkerCommands::Run { script: _ } => Ok(()),
+        },
+        Commands::Bytecode(args) => match args.command {
+            ByteCodeCommands::Run { script: _ } => bytecode::run(),
+        },
+    }
 }
