45e0378b2de2c53913b966a9f9a015c9c48de03f
Josh Karpel <josh.karpel@gmail.com>
2024-08-05 21:16:21 -0500

    wip

--- src/bytecode/scanner.rs
+++ src/bytecode/scanner.rs
@@ -1,15 +1,15 @@
 use anyhow::{anyhow, bail, Result};
 
 #[derive(Debug)]
-pub struct Scanner<'s> {
-    source: Vec<char>,
-    start: usize,   // The start of the current lexeme
-    current: usize, // The current character under consideration
+pub struct Scanner {
+    source: Vec<char>, // TODO: this is not ideal, we should be pointing back to the original source string to save memory
+    start: usize,      // The start of the current lexeme
+    current: usize,    // The current character under consideration
     line: usize,
 }
 
-impl<'s> Scanner<'s> {
-    fn new(source: &'s String) -> Scanner {
+impl Scanner {
+    fn new(source: &String) -> Scanner {
         Scanner {
             source: source.chars().collect(),
             start: 0,
@@ -26,20 +26,37 @@ impl<'s> Scanner<'s> {
             line: self.line,
         }
     }
+
+    fn is_at_end(&mut self) -> bool {
+        self.start == self.source.len()
+    }
+
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source[self.current - 1]
+    }
+
+    fn match_char(&mut self, expected: char) -> bool {
+        if self.is_at_end() || self.source[self.current] != expected {
+            false
+        } else {
+            self.current += 1;
+            true
+        }
+    }
 }
 
-impl<'s> Iterator for Scanner<'s> {
+impl Iterator for Scanner {
     type Item = Result<Token>;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.start = self.current;
 
-        if self.start == self.source.len() {
+        if self.is_at_end() {
             return None;
         }
 
-        let c = self.source.get(self.current).unwrap();
-        self.current += 1;
+        let c = self.advance();
 
         match c {
             '(' => {
@@ -75,13 +92,38 @@ impl<'s> Iterator for Scanner<'s> {
             '*' => {
                 return Some(Ok(self.make_token(TokenType::Star)));
             }
+            '!' => {
+                return Some(Ok(if self.match_char('=') {
+                    self.make_token(TokenType::BangEqual)
+                } else {
+                    self.make_token(TokenType::Bang)
+                }));
+            }
+            '=' => {
+                return Some(Ok(if self.match_char('=') {
+                    self.make_token(TokenType::EqualEqual)
+                } else {
+                    self.make_token(TokenType::Equal)
+                }));
+            }
+            '<' => {
+                return Some(Ok(if self.match_char('=') {
+                    self.make_token(TokenType::LessEqual)
+                } else {
+                    self.make_token(TokenType::Less)
+                }));
+            }
+            '>' => {
+                return Some(Ok(if self.match_char('=') {
+                    self.make_token(TokenType::GreaterEqual)
+                } else {
+                    self.make_token(TokenType::Greater)
+                }));
+            }
             _ => {}
         }
 
-        Some(Err(anyhow!(
-            "Unexpected character: {}",
-            self.source.get(self.current).unwrap()
-        )))
+        Some(Err(anyhow!("Unexpected character: {c}")))
     }
 }
 
@@ -108,6 +150,8 @@ enum TokenType {
     Star,
     Bang,
     BangEqual,
+    Equal,
+    EqualEqual,
     Greater,
     GreaterEqual,
     Less,
@@ -145,7 +189,7 @@ pub fn scan(source: &String) -> Result<()> {
                     print!("{:4} ", token.line);
                     line = token.line;
                 } else {
-                    print!("    | ");
+                    print!("   | ");
                 }
                 println!("{:?}", token);
             } else {
